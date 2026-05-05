f52487602f2260844653fe547c10749b5a444ee5
TkPAIN <129594118+TkPAIN@users.noreply.github.com>
2026-05-05 22:10:45 +0200

    Create Optimizar_v3.bat

---
+++ Optimizar_v3.bat
@@ -0,0 +1,10 @@
+@echo off
+title Kara Clan - Optimizer Vol.3 (COMPLETE / COMPLETO)
+echo.
+echo =============================================
+echo   KARA CLAN OPTIMIZER - VOL.3 (ES/EN)
+echo   Full System + Cleanup
+echo =============================================
+echo.
+echo Downloading and running as Administrator...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"irm https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v3.ps1 | iex\"' -Verb RunAs"
