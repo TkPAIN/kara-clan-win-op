0e26e7831bce195f8c554f059f556edd49d2eecb
TkPAIN <129594118+TkPAIN@users.noreply.github.com>
2026-05-08 17:50:51 +0200

    Update Optimizar_v2.bat

--- Optimizar_v2.bat
+++ Optimizar_v2.bat
@@ -1,10 +1,9 @@
 @echo off
 title Kara Clan - Optimizer Vol.2 (ADVANCED / AVANZADO)
-echo.
-echo =============================================
-echo   KARA CLAN OPTIMIZER - VOL.2 (ES/EN)
-echo   Advanced Gaming + System
-echo =============================================
-echo.
+echo ============================================
+echo KARA CLAN OPTIMIZER - VOL.2 (ES/EN)
+echo Advanced Gaming + System
+echo ============================================
 echo Downloading and running as Administrator...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"irm https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v2.ps1 | iex\"' -Verb RunAs"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"& { irm https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v2.ps1 | iex }\"'"
+pause
