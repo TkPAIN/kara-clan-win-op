7e1ee7c75b2933389fe8de18406022b37b605c85
TkPAIN <129594118+TkPAIN@users.noreply.github.com>
2026-05-08 18:42:20 +0200

    Update PowerShell command for script execution

--- Optimizar_v1.bat
+++ Optimizar_v1.bat
@@ -5,5 +5,5 @@ echo KARA CLAN OPTIMIZER - VOL.1 (ES/EN)
 echo Basic Network Optimization
 echo ====================================================
 echo Downloading and running as Administrator...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"& { irm https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v1.ps1 | iex }\"'"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$s = irm 'https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v1.ps1'; Invoke-Expression $s"
 pause
