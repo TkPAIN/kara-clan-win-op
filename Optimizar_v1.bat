8c1b22a3d36fa3ec037050428ccf0d02f948e9d0
TkPAIN <129594118+TkPAIN@users.noreply.github.com>
2026-05-08 17:50:36 +0200

    Update Optimizar_v1.bat

--- Optimizar_v1.bat
+++ Optimizar_v1.bat
@@ -1,10 +1,9 @@
 @echo off
 title Kara Clan - Optimizer Vol.1 (BASIC / ESENCIAL)
-echo.
-echo =============================================
-echo   KARA CLAN OPTIMIZER - VOL.1 (ES/EN)
-echo   Basic Network Optimization
-echo =============================================
-echo.
+echo ====================================================
+echo KARA CLAN OPTIMIZER - VOL.1 (ES/EN)
+echo Basic Network Optimization
+echo ====================================================
 echo Downloading and running as Administrator...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"irm https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v1.ps1 | iex\"' -Verb RunAs"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"& { irm https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v1.ps1 | iex }\"'"
+pause
